b28478f03f0dca7f6742c2a030482dcdc41942d1
Ana <anamkhainyambuu@gmail.com>
2017-03-09 14:57:13 -0500

    Fixed?

--- app/src/main/java/com/example/ana/busproject/BusMap.java
+++ app/src/main/java/com/example/ana/busproject/BusMap.java
@@ -1,5 +1,6 @@
 package com.example.ana.busproject;
 
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -13,6 +14,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BusMap extends FragmentActivity implements OnMapReadyCallback {
 
@@ -29,19 +35,101 @@ public class BusMap extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
     }
 
-    private void getDeviceLocation() {
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-        // A step later in the tutorial adds the code to get the device location.
-    }
-
+//    private void PerrmissionWork() {
+//
+//        List<String> permissionsNeeded = new ArrayList<String>();
+//
+//        final List<String> permissionsList = new ArrayList<String>();
+//        if (!addPermission(permissionsList,
+//                Manifest.permission.ACCESS_FINE_LOCATION))
+//            permissionsNeeded.add("GPS");
+//        if (!addPermission(permissionsList,
+//                Manifest.permission.ACCESS_COARSE_LOCATION))
+//            permissionsNeeded.add("GPS COARSE");
+//
+//
+//        if (permissionsList.size() > 0) {
+//            if (permissionsNeeded.size() > 0) {
+//                // Need Rationale
+//                String message = "You need to grant access to "
+//                        + permissionsNeeded.get(0);
+//                for (int i = 1; i < permissionsNeeded.size(); i++)
+//                    message = message + ", " + permissionsNeeded.get(i);
+//                showMessageOKCancel(message,
+//                        new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog,
+//                                                int which) {
+//                                requestPermissions(permissionsList
+//                                                .toArray(new String[permissionsList
+//                                                        .size()]),
+//                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+//                            }
+//                        });
+//                return;
+//            }
+//            requestPermissions(
+//                    permissionsList.toArray(new String[permissionsList.size()]),
+//                    REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+//            return;
+//        }
+//        splashMainWork();
+//    }
+//
+//// mapWork();
+//
+//    private boolean addPermission(List<String> permissionsList,
+//                                  String permission) {
+//        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+//            permissionsList.add(permission);
+//            // Check for Rationale Option
+//            if (!shouldShowRequestPermissionRationale(permission))
+//                return false;
+//        }
+//        return true;
+//    }
+//
+//    private void showMessageOKCancel(String message,
+//                                     android.content.DialogInterface.OnClickListener onClickListener) {
+//        new AlertDialog.Builder(context).setMessage(message)
+//                .setPositiveButton("OK", onClickListener).setCancelable(false)
+//                .setNegativeButton("Cancel", null).create().show();
+//
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,
+//                                           String[] permissions, int[] grantResults) {
+//        switch (requestCode) {
+//            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS: {
+//                Map<String, Integer> perms = new HashMap<String, Integer>();
+//                // Initial
+//                perms.put(Manifest.permission.ACCESS_FINE_LOCATION,
+//                        PackageManager.PERMISSION_GRANTED);
+//                perms.put(Manifest.permission.ACCESS_COARSE_LOCATION,
+//                        PackageManager.PERMISSION_GRANTED);
+//
+//                // Fill with results
+//                for (int i = 0; i < permissions.length; i++)
+//                    perms.put(permissions[i], grantResults[i]);
+//                // Check for ACCESS_FINE_LOCATION
+//                if (perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+//                        && perms.get(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+//                        ) {
+//                    // All Permissions Granted
+//                    splashMainWork();
+//                } else {
+//                    // Permission Denied
+//                    Toast.makeText(context, "Some Permission is Denied",
+//                            Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//            break;
+//            default:
+//                super.onRequestPermissionsResult(requestCode, permissions,
+//                        grantResults);
+//        }
+//    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
